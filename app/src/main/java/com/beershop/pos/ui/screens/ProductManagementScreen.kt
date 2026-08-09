ffc759e5a37d58124788de53fb8080887848a5b5
ttafb5-create <tta.fb5@gmail.com>
2026-08-10 01:21:52 +0630

    Add ExperimentalMaterial3Api import to suppress warning

--- app/src/main/java/com/beershop/pos/ui/screens/ProductManagementScreen.kt
+++ app/src/main/java/com/beershop/pos/ui/screens/ProductManagementScreen.kt
@@ -3,6 +3,7 @@ package com.beershop.pos.ui.screens
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
