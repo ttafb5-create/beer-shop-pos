786562b93ee288eabc407334b850f693bdbf6a0b
ttafb5-create <tta.fb5@gmail.com>
2026-08-09 22:37:14 +0630

    Fix: add missing Flow import in ReportDao.kt

--- app/src/main/java/com/beershop/pos/data/local/dao/ReportDao.kt
+++ app/src/main/java/com/beershop/pos/data/local/dao/ReportDao.kt
@@ -3,10 +3,10 @@ package com.beershop.pos.data.local.dao
 import androidx.room.*
 import com.beershop.pos.data.local.entity.DailySalesSummaryEntity
 import com.beershop.pos.data.local.entity.SettingsEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ReportDao {
-    // Daily Sales Summary
     @Query("SELECT * FROM daily_sales_summary WHERE date = :date")
     suspend fun getDailySummary(date: String): DailySalesSummaryEntity?
 
